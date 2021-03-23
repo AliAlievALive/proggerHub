5830fdd537a1a5d613e3cbab89abe916bea88075
Almir Almiev <madarat@mail.ru>
2021-03-23 21:59:43 +0300

    Подключил самодельный пакет для регистрации классов объектов

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -1,8 +1,19 @@
 package tech.itpark.proggerhub;
 
+import tech.itpark.proggerhub.di.Container;
+import tech.itpark.proggerhub.model.User;
+import tech.itpark.proggerhub.model.Video;
+import tech.itpark.proggerhub.repository.UserRepository;
+
 public class Main {
     // psvm + Tab -> main
     public static void main(String[] args) {
+
+        final Container container = new Container();
+
+        container.register(User.class, Video.class, UserRepository.class);
+        container.wire();
+        System.out.println("finish");
         // TODO: MVP -> Minimal Valuable Product
         // videos
         // users/owners <-> consumers
