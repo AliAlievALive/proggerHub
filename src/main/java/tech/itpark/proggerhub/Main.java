e56fdcb5696b1c626e76fd6ba6cf76bf71aab832
Almir Almiev <madarat@mail.ru>
2021-03-24 21:14:49 +0300

    Нужна помощь, почему то не видит библиотеку...

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -1,18 +1,15 @@
 package tech.itpark.proggerhub;
-
-import tech.itpark.proggerhub.di.Container;
 import tech.itpark.proggerhub.model.User;
 import tech.itpark.proggerhub.model.Video;
 import tech.itpark.proggerhub.repository.UserRepository;
+import tech.itpark.proggerhub.di.*;
 
 public class Main {
     // psvm + Tab -> main
     public static void main(String[] args) {
 
-        final Container container = new Container();
-
-        container.register(User.class, Video.class, UserRepository.class);
-        container.wire();
+        Container.register(User.class, Video.class, UserRepository.class);
+        Container.wire();
         System.out.println("finish");
         // TODO: MVP -> Minimal Valuable Product
         // videos
