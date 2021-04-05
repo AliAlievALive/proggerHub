3db52b370115a73f908980bfc1120e31056a94d6
Almir Almiev <madarat@mail.ru>
2021-04-05 20:13:00 +0300

    Продолжаю неудачные попытки...

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -1,25 +1,16 @@
 package tech.itpark.proggerhub;
+//import tech.itpark.proggerhub.di.Container;
 import tech.itpark.proggerhub.model.User;
 import tech.itpark.proggerhub.model.Video;
 import tech.itpark.proggerhub.repository.UserRepository;
-import tech.itpark.proggerhub.di.*;
 
 public class Main {
     // psvm + Tab -> main
     public static void main(String[] args) {
 
-        Container.register(User.class, Video.class, UserRepository.class);
-        Container.wire();
+        Container myCont = new Container();
+        myCont.register(User.class, Video.class, UserRepository.class);
+        myCont.wire();
         System.out.println("finish");
-        // TODO: MVP -> Minimal Valuable Product
-        // videos
-        // users/owners <-> consumers
-        // Domain Model -> предметная область
-
-        // 0. Plain Java (manual)
-        // 1. Dependency Lookup -> Service Registry
-        // 2. Dependency Injection -> Container:
-        // 2.1 Сам делал new
-        // 2.2 Найти нужные зависимости, если они требуются для new
     }
 }
